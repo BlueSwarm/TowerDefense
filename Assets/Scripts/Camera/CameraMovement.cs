3309f612a63af7f2238ac8b712f2b2acad296c1c
Jim <gamingodyssey2@gmail.com>
2013-05-24 16:09:40 +0300

    Comment Updates
    Added detailed comments and an update note. Be sure to check the update
    note for more info.

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour 
 {
+	// The initial speed in which the camera moves.
+	// I made it public so that I can change it directly from the editor.
+	// Try experimenting around with the numbers.
+	// I found out that 50+ is a good start.
 	public float speed = 5.0f;
 	
+	// Storing the moving direction in a Vector3 ensures a simple move method.
+	// The only component from x,y,z that will be != 0 is going to be the
+	// desired direction. Also, y will always be 0.
 	private Vector3 direction;
 	
 	void Start ()
@@ -12,12 +19,18 @@ public class CameraMovement : MonoBehaviour
 		direction = new Vector3 ();
 	}
 	
+	// Side note: I like putting simple things, like 1 line of code into
+	// separate functions. It helps me design the algorithm by just putting
+	// function names in order in the Update function.
 	void Update ()
 	{
 		DetectDirection ();
 		Move ();
 	}
 	
+	// Pretty straight forward function. It resets the direction to 0,
+	// gets the mouse position and checks to see if we are at the edge of the screen.
+	// If so, we change the direction!
 	void DetectDirection ()
 	{
 		direction = Vector3.zero;
@@ -43,6 +56,19 @@ public class CameraMovement : MonoBehaviour
 		}
 	}
 	
+	// Move only consists of 1 line of code which as the name says,
+	// moves the camera transform.
+	// transform is an instance of Transform for the game object that
+	// we attached the script on. That is handled by the Unity Engine
+	// so we don't have to declare anything!
+	// If you open the Unity editor and you choose any object,
+	// you'll notice on the top of the inspector a section called
+	// transform. This is what this instance holds.
+	// We don't need physics movement for the camera, so we just
+	// adjust it's current position by adding the direction,
+	// multiplied by our speed and by Time.deltaTime.
+	// The delta time is the time passed between this frame and the last.
+	// This ensures a smooth movement, when the game has some frame issues.
 	void Move ()
 	{
 		transform.position += direction * speed * Time.deltaTime;
--- Assets/Scripts/Camera/CameraZoom.cs
+++ Assets/Scripts/Camera/CameraZoom.cs
@@ -3,14 +3,15 @@ using System.Collections;
 
 public class CameraZoom : MonoBehaviour 
 {
+	// Same with movement but this time try a number of
+	// 100+. I had a weird bug earlier where I had a value of
+	// 100 and it was going fast enough, but when I changed
+	// to 500 it was going slower...it went away afterwards.
 	public float speed = 10.0f;
 	
+	// This time the direction is only counted on Y-axis so
+	// we don't need a Vector3.
 	private float direction;
-	
-	void Start () 
-	{
-		
-	}
 	
 	void Update ()
 	{
@@ -18,6 +19,11 @@ public class CameraZoom : MonoBehaviour
 		Move ();
 	}
 	
+	// This is a lot simpler. Input.GetAxis (NAME_OF_AXIS)
+	// checks if a specific axis is being used. It also returns
+	// the direction but it's reversed from what we want, hence
+	// the -! I'll have to refine this a bit more, cause I suspsect
+	// this is the cause of the weird bug I mentioned earlier.
 	void DetectDirection ()
 	{
 		direction = -Input.GetAxis ("Mouse ScrollWheel");
