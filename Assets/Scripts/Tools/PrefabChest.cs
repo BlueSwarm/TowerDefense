dabbc1c6c6103420bf482ec383e24fe4ea69496e
Jim <gamingodyssey2@gmail.com>
2013-05-28 17:23:16 +0300

    Update 3
    Make sure to check Update 3.txt in Update Notes!

---
+++ Assets/Scripts/Tools/PrefabChest.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using System.Collections;
+
+public class PrefabChest : MonoBehaviour 
+{
+	public Transform towerPrefab;
+}
--- Assets/Scripts/Tower/HolderUtilities.cs
+++ Assets/Scripts/Tower/HolderUtilities.cs
@@ -4,8 +4,14 @@ using System.Collections;
 public class HolderUtilities : MonoBehaviour 
 {
 	public bool holds = false;
-
+	
+	private PrefabChest pcInstance;
 	private Transform tower;
+		
+	void Start ()
+	{
+		pcInstance = GameObject.Find ("MapLogic").GetComponent<PrefabChest> ();
+	}
 	
 	public void HoldTower (string tower)
 	{
@@ -25,14 +31,38 @@ public class HolderUtilities : MonoBehaviour
 		}
 		
 		// Asign the tower and create the game object
-		// coming soon!
+		// Get the prefab from the chest.
+		AccesssPrefabChest (tower);
+		
+		// Instantiate the tower.
+		// Instantiate takes the current prefab and creates a
+		// game object of it at the given location with the
+		// given rotation. Because we don't want to change the
+		// rotation but we have to give a new rotation to the
+		// function, we simply use Quaternion.identity, which
+		// is no rotation!
+		Vector3 towerPosition = transform.position + new Vector3 (0, 3, 0);
+		this.tower = (Transform) Instantiate (this.tower, towerPosition, Quaternion.identity);
+		
 		holds = true;
+	}
+	
+	private void AccesssPrefabChest (string tower)
+	{		
+		if (tower == "Tower 1")
+		{
+			this.tower = pcInstance.towerPrefab;
+		}
 	}
 	
 	public void RemoveTower ()
 	{		
 		// Destroy the tower
-		GameObject.Destroy (tower);
+		if (!holds)
+		{
+			return;
+		}
+		GameObject.Destroy (tower.gameObject);
 		holds = false;
 		
 		// Enable particles
